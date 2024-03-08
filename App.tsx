79d540a3df7f4acfc62d9ad9c8ad7e4d6103ed38
eoo2 <eoo2@njit.edu>
2024-03-08 12:29:58 -0500

    I am gettin a hang of it

--- App.tsx
+++ App.tsx
@@ -1,6 +1,11 @@
+
 import { Tabs, TabList, Tab, TabPanels } from '@chakra-ui/react'
+import { Image } from '@chakra-ui/react'
+import { Box } from '@chakra-ui/react'
 
 export const App = () => (
+  <div>
+    {/* Tabs component */}
   <Tabs height="42px" alignSelf="stretch">
     <TabList>
       <Tab>Home</Tab>
@@ -9,4 +14,26 @@ export const App = () => (
     </TabList>
     <TabPanels />
   </Tabs>
-)
+
+  {/* Image component */}
+  <Image
+    src="null"
+    alt="INSERT_ALT"
+    width="100%"
+    height="498px"
+    maxWidth="100%"
+    objectFit="cover"
+  />
+
+{/* Box component */}
+  <Box
+    borderRadius="15px"
+    width="494px"
+    height="221px"
+    maxWidth="100%"
+    background="rgba(113, 128, 150, 0.5)"
+  />
+  </div>
+);
+
+export default App;
