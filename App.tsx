eb852d9dc8b1f6fa83062fdebdc78c6f4bba7ab3
eoo2 <eoo2@njit.edu>
2024-03-08 13:13:47 -0500

    looking good

--- App.tsx
+++ App.tsx
@@ -2,6 +2,10 @@
 import { Tabs, TabList, Tab, TabPanels } from '@chakra-ui/react'
 import { Image } from '@chakra-ui/react'
 import { Box } from '@chakra-ui/react'
+import { Input } from '@chakra-ui/react'
+import { Button } from '@chakra-ui/react'
+import { Text } from '@chakra-ui/react'
+
 
 export const App = () => (
   <div>
@@ -33,6 +37,68 @@ export const App = () => (
     maxWidth="100%"
     background="rgba(113, 128, 150, 0.5)"
   />
+{/* Text component */}
+  <Text
+    fontFamily="Inter"
+    lineHeight="1"
+    fontWeight="bold"
+    fontSize="48px"
+    color="#FFFFFF"
+  >
+    Your Experience, Their Future
+  </Text>
+
+  <Text
+    fontFamily="Inter"
+    lineHeight="1.2"
+    fontWeight="bold"
+    fontSize="20px"
+    color="#FFFFFF"
+  >
+    Propose a Guest Lecture for the WIS Club
+  </Text>
+  <Text
+    fontFamily="Inter"
+    lineHeight="1.2"
+    fontWeight="bold"
+    fontSize="30px"
+    color="black"
+  >
+    Real-World Education
+  </Text>
+  <Text
+    fontFamily="Inter"
+    lineHeight="1.5"
+    fontWeight="regular"
+    fontSize="16px"
+    color="black"
+    width="652px"
+    maxWidth="100%"
+  >
+    We believe true learning extends beyond the classroom. The WIS Club
+    facilitates connections between students and industry professionals, giving
+    students access to invaluable real-world expertise. Through guest lectures,
+    office tours and more, we provide opportunities for students to gain
+    insights, get career advice, and build their professional networks.
+  </Text>
+
+
+  {/* Input component */}
+    <Input
+    placeholder="your email address"
+    size="sm"
+    width="345px"
+    height="32px"
+    maxWidth="100%"
+  />
+
+{/* Button component */}
+  <Button size="lg" colorScheme="blue" width="101px" height="48px">
+    Sign up
+  </Button>
+
+
+
   </div>
 );
 
